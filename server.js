d6a046c7c6e707737b976f293fb746695b6f7f60
Luiz Silva <luiz@oromus.com.br>
2015-03-18 07:01:07 -0300

    start script added for heroku

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ var express = require('express'),
 	http = require('http').Server(app),
 	path = require('path');
 
+
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.use('/', express.static(path.join(__dirname,'public')));
 
 
-http.listen('3000', function(){
+http.listen(port, function(){
 	console.log("server started");
 });
